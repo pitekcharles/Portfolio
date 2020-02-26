29e6b2a8ba5c008cf11614c68ae094fa27913a7c
Noodles <charles.pitek@gmail.com>
2020-02-25 19:15:35 -0700

    created card text component and added it to the card wrapper

---
+++ src/Components/CardText/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CardText() {
+    return (
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    );
+}
+
+export default CardText;
--- src/Components/CardWrapper/index.js
+++ src/Components/CardWrapper/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardTitle from "../CardTitle";
+import CardText from "../CardText";
 
 function CardWrapper() {
     return (
         <div class="card">
             <div class="card-body">
                 <CardTitle />
+                <CardText />
             </div>
         </div>
     )
