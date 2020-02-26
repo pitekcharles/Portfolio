22d72fcbba36f80c28336d3d0a6d70216b0a80f0
Noodles <charles.pitek@gmail.com>
2020-02-25 18:14:03 -0700

    added jumbotron headline component and added it to the jumbotron wrapper

---
+++ src/Components/JumbotronHeadline/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function JumbotronHeadline(){
+    return (
+        <h1 class="display-4">Charles Pitek</h1>
+    )
+}
+
+export default JumbotronHeadline;
--- src/Components/JumbotronWrapper/index.js
+++ src/Components/JumbotronWrapper/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import JumbotronHeadline from "../JumbotronHeadline";
 
 function JumbotronWrapper() {
     return (
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
+                <JumbotronHeadline />
                 <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
             </div>
         </div>
