f27eb95fd9a48ee2f12139089d59c59c29cc91b2
Noodles <charles.pitek@gmail.com>
2020-02-25 17:06:46 -0700

    created basic NavItem component and altered the Nav index to use NavItem

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NavItem from "../NavItem";
 
 function Nav() {
     return (
@@ -9,18 +10,10 @@ function Nav() {
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
+                    <NavItem />
+                    <NavItem />
+                    <NavItem />
+                    <NavItem />
                 </ul>
             </div>
         </nav>
---
+++ src/Components/NavItem/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavItem() {
+    return (
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+    )
+}
+
+export default NavItem;
