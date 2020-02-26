c2efc0bd40f6f6c98b0479ade4b5175df1b5a344
Noodles <charles.pitek@gmail.com>
2020-02-25 18:08:32 -0700

    Created jumbotron wrapper element and added it to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./Components/Nav"
+import JumbotronWrapper from "./Components/JumbotronWrapper";
 
 function App() {
     return (
         <div>
             <Nav />
+            <JumbotronWrapper />
         </div>
     );
 }
